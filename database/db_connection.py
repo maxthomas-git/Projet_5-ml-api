0914f0e77e137db4a925a57a71e9552ca156ae88
Maximilien <maximilien_thomas@yahoo.com>
2026-06-27 17:12:45 +0200

    modification db_connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -11,7 +11,7 @@ if ENV in ["ci", "hf"]:
 
 elif ENV == "local":
     DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
+    DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
